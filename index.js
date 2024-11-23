5d63e92cf1053c66c315c91267c7cd8f6ee41dac
jeycee <jeyceejeyka635@gmail.com>
2024-11-23 21:59:20 +0300

    Adds arithmatic functions

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a + b;
+  }
+  add(2, 3); 
+  function subtract(a, b) {
+    return a - b;
+  }
+  subtract(6, 3); 
+  function multiply(a, b) {
+    return a * b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  multiply(3, 6); 
+  function divide(a, b) {
+    return a / b;
+  }
+  divide(12, 4);       
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
